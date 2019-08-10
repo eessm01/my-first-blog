7ad1b732037cc533faeda07052b2c828879954f6
eessm01 <eessm01@gmail.com>
2019-08-10 10:42:09 -0500

    Actualización, login, logout y drafft list

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# views
+from django.contrib.auth import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
