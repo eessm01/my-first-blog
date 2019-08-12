ab8e12fc997fffe810174a482a8349405d6035b8
eessm01 <eessm01@gmail.com>
2019-08-11 21:01:44 -0500

    Versión final de Djangogirls tutorial

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 # models 
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -10,3 +10,9 @@ class PostForm(forms.ModelForm):
     model = Post
     fields = ('title', 'text',)
     
+
+class CommentForm(forms.ModelForm):
+
+  class Meta:
+    model = Comment
+    fields = ('author', 'text',)
